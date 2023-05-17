3e1d75c2c6cf48c895e77e17e55099683dfc7c4a
DamianKonc <damian.koncewicz2@gmail.com>
2023-05-17 21:11:26 +0200

    added fields in starShipUrl

--- app/starships/[starShipUrl]/page.tsx
+++ app/starships/[starShipUrl]/page.tsx
@@ -23,6 +23,45 @@ export default async function StarShipPage({params: {starShipUrl}}: Params) {
 
     console.log(starShip)
   return (
-    <h2>{starShip.name}</h2>
+    <>
+    <h2>name: {starShip.name}</h2>
+    <p>model: {starShip.model}</p>
+    <p>manufacturer: {starShip.manufacturer}</p>
+    <p>cost_in_credits: {starShip.cost_in_credits}</p>
+    <p>length: {starShip.length}</p>
+    <p>max_atmosphering_speed: {starShip.max_atmosphering_speed}</p>
+    <p>crew: {starShip.crew}</p>
+    <p>passengers: {starShip.passengers}</p>
+    <p>cargo_capacity: {starShip.cargo_capacity}</p>
+    <p>consumables: {starShip.consumables}</p>
+    <p>hyperdrive_rating: {starShip.hyperdrive_rating}</p>
+    <p>MGLT: {starShip.MGLT}</p>
+    <p>starship_class: {starShip.starship_class}</p>
+    {starShip.pilots && <p>starship_class: {starShip.pilots}</p>}
+    <p>films: {starShip.films}</p>
+    </>
   )
 }
+
+
+// name: 'Rebel transport',
+// model: 'GR-75 medium transport',
+// manufacturer: 'Gallofree Yards, Inc.',
+// cost_in_credits: 'unknown',
+// length: '90',
+// max_atmosphering_speed: '650',
+// crew: '6',
+// passengers: '90',
+// cargo_capacity: '19000000',
+// consumables: '6 months',
+// hyperdrive_rating: '4.0',
+// MGLT: '20',
+// starship_class: 'Medium transport',
+// pilots: [],
+// films: [
+//   'https://swapi.dev/api/films/2/',
+//   'https://swapi.dev/api/films/3/'
+// ],
+// created: '2014-12-15T12:34:52.264000Z',
+// edited: '2014-12-20T21:23:49.895000Z',
+// url: 'https://swapi.dev/api/starships/17/'
