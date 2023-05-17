83f28601168b6cc28d6257cf1886023194c7e5f6
DamianKonc <damian.koncewicz2@gmail.com>
2023-05-17 20:54:20 +0200

    fetching data

---
+++ lib/getShip.tsx
@@ -0,0 +1,8 @@
+
+export default async function getShip(starshipUrl: string) {
+    const res = await fetch(starshipUrl)
+
+    if(!res.ok) throw new Error('failed to fetch ship') 
+
+  return res.json()
+}
