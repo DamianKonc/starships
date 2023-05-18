d78cbb9e28ef63d319b86e7b8cbf5654ad3f3e94
DamianKonc <damian.koncewicz2@gmail.com>
2023-05-18 11:41:03 +0200

    fixed types

--- app/components/DropDown/DropDown.tsx
+++ app/components/DropDown/DropDown.tsx
@@ -1,17 +1,18 @@
 "use client"
 
-import React from 'react'
+import React, { Dispatch, SetStateAction } from 'react'
 import { useState } from 'react'
+import { Film } from '@/types'
 
 
-export default function DropDown({filmsArray, pickedMovie, setPickedMovie}) {
-    const [isOpen, setIsOpen] = useState(false)
+export default function DropDown({filmsArray, pickedMovie, setPickedMovie}:{filmsArray: Film[], pickedMovie: Film | {title: string, starships: []}, setPickedMovie: Dispatch<SetStateAction<Film | { title: "All"; starships: []; }>>}) {
+  const [isOpen, setIsOpen] = useState(false)
 
     const handleOpen = () => {
       setIsOpen((prev)=>!prev)
     }
 
-    const handePickedMovie = (data) => {
+    const handePickedMovie = (data: SetStateAction<Film | { title: "All"; starships: []; }>) => {
       setPickedMovie(data)
       handleOpen()
     }
@@ -20,7 +21,7 @@ export default function DropDown({filmsArray, pickedMovie, setPickedMovie}) {
         <button onClick={handleOpen}>{pickedMovie.title}</button>
         <div style={{display: isOpen ? "block":"none"}}>
             <ul className="py-2 text-sm text-gray-700 dark:text-gray-200" aria-labelledby="dropdownDefaultButton">
-            <li onClick={()=>handePickedMovie({title: "All"})}>
+            <li onClick={()=>handePickedMovie({title: "All", starships: []})}>
                 <p >All</p>
             </li>
             {filmsArray.map(film=>(<li key={film.title} onClick={()=>handePickedMovie(film)}>
--- app/components/StarShipsBody/StarShipsBody.tsx
+++ app/components/StarShipsBody/StarShipsBody.tsx
@@ -3,17 +3,20 @@
 import Link from "next/link"
 import DropDown from "../DropDown/DropDown"
 import { useState, useEffect } from "react"
+import { Film, StarShip } from "@/types"
 
 
-export default  function StarShipsBody({films}) {
-    const [ pickedMovie, setPickedMovie] = useState({title: "All"});
-    const [filteredStarships, setFilteredStarships] = useState([])
-    const [starships, setStarships] = useState([]);
+export default  function StarShipsBody({films}:{films: Film[]}) {
+    const [pickedMovie, setPickedMovie] = useState<Film | {title: "All", starships: []}>({title: "All", starships:[]});
+    const [filteredStarships, setFilteredStarships] = useState<StarShip[] | []>([])
+    const [starships, setStarships] = useState<StarShip[] | []>([]);
+
+    console.log(starships)
 
 
     useEffect(() => {
         const fetchStarships = async () => {
-          let allStarships = [];
+          let allStarships: any[] = [];
           let nextUrl = 'https://swapi.dev/api/starships/';
     
           while (nextUrl) {
@@ -44,16 +47,12 @@ export default  function StarShipsBody({films}) {
         setFilteredStarships([]);
         filteringShips();
       }, [pickedMovie]);
-      
-      useEffect(() => {
-        console.log(filteredStarships);
-      }, [filteredStarships]);
 
      return (
     <div>
     <DropDown filmsArray={films} pickedMovie={pickedMovie} setPickedMovie={setPickedMovie}/>
     <br/>
-    {pickedMovie.starships? filteredStarships.map(starShip=>{
+    {pickedMovie.starships.length>0? filteredStarships.map(starShip=>{
         
         return(
             <p key={starShip.url}>
